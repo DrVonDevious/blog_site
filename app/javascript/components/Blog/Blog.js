cd18e4b833c8a1cf85e96214645abe285d7c249b
DrVonDevious <drvondevious@gmail.com>
2020-01-25 23:42:46 -0600

    Restructure Blog.js

--- app/javascript/components/Blog/Blog.js
+++ app/javascript/components/Blog/Blog.js
@@ -1,12 +1,57 @@
 
 import React, { Component } from 'react'
-import Jumbotron from './Jumbotron'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import Body from './Body'
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		background-color: #202020;
+	}	
+`
+
+const Section = styled.section`
+        background-color: #353535;
+        color: #bb4302;
+        min-height: 80px;
+        margin: -15px;
+        padding: 4px;
+        box-shadow: 5px 10px 10px #101010;
+`
+
+const H1 = styled.h1`
+        text-align: center;
+        font-size: 40px;
+        text-shadow: 1px 1px 4px #000000;
+`
+
+const Jumbotron = () => {
+
+        return (
+                <Section className="header-section">
+                        <div className="container">
+                                <div className="row">
+                                        <div className="col">
+                                                <div className="site-name">
+                                                        <H1>Devious Programmer</H1>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </Section>
+        )
+
+}
 
 class Blog extends Component {
 
 	render() {
 		return (
-			<div><Jumbotron/></div>
+			<div>
+				<GlobalStyle/>
+				<Jumbotron/>
+				<Body/>
+			</div>
 		)
 	}
 }
